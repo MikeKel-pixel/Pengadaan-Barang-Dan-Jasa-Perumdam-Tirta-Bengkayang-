be2917a39c7b870a57f284e1f4ed3793a8f8ee38
MikeKel-pixel <appleboygaming.57@gmail.com>
2026-07-10 23:28:36 +0700

    feat: konfigurasi final deploy Laravel ke Vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,14 @@
 <?php
-/**
-* Here is the serverless function entry
-* for deployment with Vercel.
-*/
-require __DIR__.'/../public/index.php';
+
+// Load autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load environment
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Boot Laravel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
